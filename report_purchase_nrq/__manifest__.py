da18cea981cd80fa2a640a24d4abf41e8ae23d11
AaronCTL <aaron.chan@roomsfor.hk>
2017-02-01 11:47:51 +0800

    test

---
+++ report_purchase_nrq/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Rooms For (Hong Kong) Limited T/A OSCG
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Purchase Document Print",
+    "summary": "",
+    "version": "10.0.1.0.0",
+    "category": "Purchases",
+    "website": "https://www.odoo-asia.com/",
+    "author": "Rooms For (Hong Kong) Limited T/A OSCG",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "pre_init_hook": "",
+    "post_init_hook": "",
+    "post_load": "",
+    "uninstall_hook": "",
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "purchase",
+    ],
+    "data": [
+        'report/purchase_report_template.xml'
+    ],
+    "demo": [
+    ],
+    "qweb": [
+    ]
+}
